ba37be200c007c2b7c9ea7e022393db8e0a617e0
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-05-11 10:52:29 +0200

    Récupération éléments opérationnelle

--- main.js
+++ main.js
@@ -10,6 +10,17 @@ fetch("http://localhost:3000/api/furniture")
 .then( jsonListFurniture => {
     for (let jsonFurniture of jsonListFurniture) {
         let furniture = new Furniture(jsonFurniture);
-        document.getElementById("affichageElements").innerHTML += ""
+        document.getElementById("tm-gallery-page-pizza").innerHTML +=
+                    `<article class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
+                    <figure>
+                        <img src="${furniture.imageUrl}" alt="Photo de la ${furniture.name}" class="img-fluid tm-gallery-img" />
+                        <figcaption>
+                            <h4 class="tm-gallery-title">${furniture.name}</h4>
+                            <p class="tm-gallery-description">${furniture.description}</p>
+                            <p class="tm-gallery-price">${furniture.price} €</p>
+                        </figcaption>
+                    </figure>
+                </article>
+                    `;
     }
 });
