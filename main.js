35c07cd9f802f2e46e93a85c876649275797ea06
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-05-13 19:44:17 +0200

    améliorations visuelles - vignettes non cliquables

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ fetch("http://localhost:3000/api/furniture")
 .then( jsonListFurniture => {
     for (let jsonFurniture of jsonListFurniture) {
         let furniture = new Furniture(jsonFurniture);
-        document.getElementById("tm-gallery-page-pizza").innerHTML +=
-                    `<article class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
+        document.getElementById("affichageDesFurnitures").innerHTML +=
+                    `<article class="vignetteProduit">
                     <figure>
-                        <img src="${furniture.imageUrl}" alt="Photo de la ${furniture.name}" class="img-fluid tm-gallery-img" />
+                        <img src="${furniture.imageUrl}" alt="Photo du modèle ${furniture.name}" class="image__vignetteProduit" />
                         <figcaption>
-                            <h4 class="tm-gallery-title">${furniture.name}</h4>
-                            <p class="tm-gallery-description">${furniture.description}</p>
-                            <p class="tm-gallery-price">${furniture.price} €</p>
+                            <h4 class="titre__vignetteProduit">${furniture.name}</h4>
+                            <p class="description__vignetteProduit">${furniture.description}</p>
+                            <p class="prix__vignetteProduit">${furniture.price} €</p>
                         </figcaption>
                     </figure>
                 </article>
